d3947f7641a8e9d37cc5195f193fea275927b575
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-06-15 19:15:01 -0300

    Manipulação de D.O.M

---
+++ JavaScript/DOM/assets/js/script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById('mode-selector')
+const h1 = document.getElementById('page-title')
+const body = document.getElementsByTagName('body')[0]
+const footer = document.getElementsByTagName('footer')[0]
+
+function changeMode() {
+    changeClass()
+    changeText()
+}
+
+function changeClass() {
+    button.classList.toggle('dark-mode')
+    h1.classList.toggle('dark-mode')
+    body.classList.toggle('dark-mode')
+    footer.classList.toggle('dark-mode')
+}
+
+function changeText() {
+    if(body.classList.contains('dark-mode')) {
+        button.innerText = 'Light Mode'
+        h1.innerHTML = ' Dark Mode ON'
+        return
+    }
+    button.innerText = 'Dark Mode'
+    h1.innerHTML = ' Light Mode ON'
+}
+button.addEventListener('click', changeMode)
