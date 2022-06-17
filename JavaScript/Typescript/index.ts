c3783184f8d4a1dd88f28b3ec9da3d87d487645f
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-06-17 08:54:13 -0300

    TypeScript sem modulos

--- JavaScript/Typescript/index.ts
+++ JavaScript/Typescript/index.ts
@@ -1,4 +1,48 @@
-function soma(a: number, b: number){
-    return a + b;
+// function soma(a: number, b: number){
+//     return a + b;
+// }
+// soma(1, 2);
+//interfaces
+/*interface Animal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático' | 'anfíbio';
+    //executarRugido(Decibels: number): void;
 }
-soma(1, 2);
+interface Felinos extends Animal {
+    visaoNoturna: boolean;
+
+}
+const animal: Animal = {
+    nome: 'Cachorro',
+    tipo: 'terrestre'
+    //executarRugido: (Decibels) => (`${Decibels}dB`)
+} 
+//animal.executarRugido(10)
+const felino: Felinos = {
+    nome: 'Cachorro',
+    tipo: 'terrestre',
+    visaoNoturna: false
+}*/
+//Types
+type Animal = {
+    nome: string;
+    tipo: 'terrestre' | 'aquático' | 'anfíbio';
+    domesticado: boolean;
+}
+
+interface Felinos extends Animal {
+    visaoNoturna: boolean;
+}
+
+interface Canino extends Animal {
+    porte: 'pequeno' | 'médio' | 'grande';
+}
+
+type Domestico = Felinos | Canino;
+
+const animal: Domestico = {
+    domesticado: true,
+    nome: 'Cachorro',
+    porte: 'pequeno',
+    tipo: 'terrestre'
+}
