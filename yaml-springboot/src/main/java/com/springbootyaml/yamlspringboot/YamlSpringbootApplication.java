ce98f39ba2265fa5a590ce09883ab0334d98882a
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-07-27 14:51:34 -0300

    update

---
+++ yaml-springboot/src/main/java/com/springbootyaml/yamlspringboot/YamlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootyaml.yamlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlSpringbootApplication.class, args);
+	}
+
+}
---
+++ yaml-springboot/src/test/java/com/springbootyaml/yamlspringboot/YamlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootyaml.yamlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
