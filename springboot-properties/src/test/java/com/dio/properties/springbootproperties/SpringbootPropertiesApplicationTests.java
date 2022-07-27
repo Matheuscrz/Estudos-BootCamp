5395b888387d494adf42f736d361a2617423d39a
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-07-27 13:28:19 -0300

    SpringBoot

---
+++ springboot-properties/src/test/java/com/dio/properties/springbootproperties/SpringbootPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.properties.springbootproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springboot-yaml/src/test/java/com/springboot/yaml/springbootyaml/SpringbootYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.yaml.springbootyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
