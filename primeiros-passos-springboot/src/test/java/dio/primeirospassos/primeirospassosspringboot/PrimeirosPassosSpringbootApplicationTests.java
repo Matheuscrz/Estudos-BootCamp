f8f933f4376e58368b061e131d1157e311bfc3ad
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-07-27 20:31:32 -0300

    update

---
+++ primeiros-passos-springboot/src/test/java/dio/primeirospassos/primeirospassosspringboot/PrimeirosPassosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.primeirospassos.primeirospassosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
